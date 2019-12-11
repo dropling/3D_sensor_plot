9abfdfb9b9bb0d0c7ea32201899813f3d44312ca
dropling <markus.gertig@web.de>
2019-12-11 18:35:21 +0100

    added functionality to load room data and sensors from files via the menu. now starts thread for plotting as soon as enough data is available. program freezes due to waiting times for thread to stop.

--- src/window_main.py
+++ src/window_main.py
@@ -1,6 +1,9 @@
 import tkinter as tk
+#import tkFileDialog
+from tkinter import filedialog
 from matplotlib.figure import Figure
 import time
+import threading
 
 try:
     from class_plotter import Plotter
@@ -29,6 +32,11 @@ class Main_window(tk.Tk):
         self.bool_plot = True
         self.create_widgets()
         self.bind("<<event_refresh_canvas>>", self.refresh_canvas)
+        self.sensors = False
+        self.room = False
+        self.stop_thread_plot_procedures = 0
+        self.figure_refresher = None
+        self.toolbar = None
         
     def create_widgets(self):
         # create a menu
@@ -41,7 +49,7 @@ class Main_window(tk.Tk):
         filemenu = tk.Menu(menu)
         menu.add_cascade(label="File", menu=filemenu)
         filemenu.add_command(label="New File - Room", command=lambda: self.callback())
-        filemenu.add_command(label="Open File - Room", command=lambda: self.callback())
+        filemenu.add_command(label="Open File - Room", command=lambda: self.load_room())
         filemenu.add_separator()
         filemenu.add_command(label="Exit", command=lambda: self.exit())
         self.file_room_open = None
@@ -49,7 +57,7 @@ class Main_window(tk.Tk):
         sensormenu = tk.Menu(menu)
         menu.add_cascade(label="Sensors", menu=sensormenu)
         sensormenu.add_command(label="New File - Sensors", command=lambda: self.callback())
-        sensormenu.add_command(label="Open File - Sensors", command=lambda: self.callback())
+        sensormenu.add_command(label="Open File - Sensors", command=lambda: self.load_sensors())
         sensormenu.add_command(label="Save Sensors", command=lambda: self.callback())
         sensormenu.add_separator()
         sensormenu.add_command(label="New Sensor", command=lambda: self.window_open_new_sensor())
@@ -61,7 +69,7 @@ class Main_window(tk.Tk):
         helpmenu.add_command(label="About...", command=lambda: self.callback())
         
         # Canvas for plots
-        self.fig = Figure(figsize=(6, 4), dpi=100)
+        self.fig = Figure(figsize=(6, 4), dpi=100) # empty figure for spacing
         self.canvas = FigureCanvasTkAgg(self.fig, master=self)
         self.canvas.draw()
         self.canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=True)
@@ -71,14 +79,15 @@ class Main_window(tk.Tk):
         self.canvas.draw()
         
     def thread_plot_procedures(self):
-        time.sleep(1)
-        
+        self.stop_thread_plot_procedures = 0
         self.ax = None
         self.cb = None
         self.sensor_scatter = None
         self.sensor_scatter_na = None
         self.normalize = None
-        self.toolbar = NavigationToolbar2Tk(self.canvas, self)
+        self.fig.clf()
+        if(self.toolbar == None):
+            self.toolbar = NavigationToolbar2Tk(self.canvas, self)
         self.ax, self.cb, self.normalize = self.plotter.plot_room(self.fig,self.ax,self.cb)
         self.canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=1)
         self.toolbar.update()
@@ -87,20 +96,64 @@ class Main_window(tk.Tk):
         
         while(self.bool_plot):
             self.ax, self.cb, self.sensor_scatter, self.sensor_scatter_na = self.plotter.plot_sensor_data_only(self.fig, self.ax, self.cb, self.sensor_scatter, self.sensor_scatter_na, self.normalize)
-            self.event_generate("<<event_refresh_canvas>>")
+            if(self.stop_thread_plot_procedures == 1):
+                break
+            self.event_generate("<<event_refresh_canvas>>") # if app is closed here, python will freeze indefinetly
+            if(self.stop_thread_plot_procedures == 1):
+                break
+        self.figure_refresher = None
         
         # Callback functions
     def window_open_new_sensor(self):
         window = New_sensor_window(self)
         return
-        
+    
+    def wait_for_figure_refresher_thread_to_end(self):
+        while(self.figure_refresher != None):
+            time.sleep(0.25)
+        return True
+    
+    def kill_figure_refresher(self):
+        self.stop_thread_plot_procedures = 1
+        return
+    
+    def start_figure_refresher(self):
+        if(self.figure_refresher == None):
+            self.figure_refresher = threading.Thread(target=self.thread_plot_procedures)
+            self.figure_refresher.start()
+    
+    def load_room(self):
+        file_name = filedialog.askopenfilename(title = "Select file for room",
+                                                 filetypes = (("json files","*.json"),("all files","*.*")))
+        self.plotter.load_points(file_name)
+        self.room = True
+        if self.room and self.sensors:
+            if(self.figure_refresher):
+                self.kill_figure_refresher()
+            if(self.wait_for_figure_refresher_thread_to_end()):
+                self.start_figure_refresher()
+            
+    
+    def load_sensors(self):
+        file_name = filedialog.askopenfilename(title = "Select file for sensors",
+                                                 filetypes = (("json files","*.json"),("all files","*.*")))
+        self.plotter.load_sensors(file_name)
+        self.plotter.initialize_sensor_handlers()
+        self.sensors = True
+        if self.room and self.sensors:
+            if(self.figure_refresher):
+                self.kill_figure_refresher()
+            if(self.wait_for_figure_refresher_thread_to_end()):
+                self.start_figure_refresher()
+            
     def callback(self):
         print("called the callback!")
         return
 
     def exit(self):
-        self.bool_plot = False
-        time.sleep(2)
+        if(self.figure_refresher):
+            self.kill_figure_refresher()
+        self.wait_for_figure_refresher_thread_to_end()
         self.save_state()
         self.destroy()
         
